64113c6a35c9bfb3e293ebbe94fc94eb16e90a8f
Paul Reioux <reioux@gmail.com>
2014-12-06 19:41:35 -0600

    dm-crypt: use 2x number of write threads to saturate each cpus pipeline
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -1648,7 +1648,7 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 
 	cc->crypt_queue = alloc_workqueue("kcryptd",
 					  WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM |
-					  WQ_UNBOUND, num_online_cpus());
+					  WQ_UNBOUND, num_possible_cpus() * 2);
 	if (!cc->crypt_queue) {
 		ti->error = "Couldn't create kcryptd queue";
 		goto bad;
