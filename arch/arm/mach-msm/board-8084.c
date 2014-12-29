b466ba7c81dbd57fd29882156835f816f6a9dde9
flar2 <asegaert@gmail.com>
2014-12-28 21:16:17 -0500

    msm_thermal: throttle cpu freq at high temps
    Clean and simple, thanks @franciscofranco
    Based on https://github.com/franciscofranco/hammerhead/commit/ce8a51b1eeedb0d11a1df3f7e74375e836ba88dd

--- arch/arm/mach-msm/board-8084.c
+++ arch/arm/mach-msm/board-8084.c
@@ -132,7 +132,6 @@ void __init apq8084_add_drivers(void)
 	else
 		msm_clock_init(&apq8084_clock_init_data);
 	tsens_tm_init_driver();
-	msm_thermal_device_init();
 #if defined(CONFIG_LCD_KCAL)
 	shamu_add_lcd_kcal_devices();
 #endif
