50b5aa66546bc9e43f9f9238b7e448c34bf4d5ef
BitO BSessiOn <bitobsession@gmail.com>
2016-01-24 18:24:27 +0100

    arm: LLVMLinux: use static inline in ARM ftrace.h
    With compilers which follow the C99 standard (like modern versions of gcc and
    clang), "extern inline" does the wrong thing (emits code for an externally
    linkable version of the inline function). In this case using static inline
    and removing the NULL version of return_address in return_address.c does
    the right thing.
    
    Signed-off-by: Behan Webster <behanw@converseincode.com>
    Reviewed-by: Mark Charlebois <charlebm@gmail.com>
    
    Change-Id: Ic34afc86229a4255b62a48dcf0808bf0d3ce3474
    source: http://www.serverphorums.com/read.php?12,880351,880351#msg-880351

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -62,12 +62,12 @@ void *return_address(unsigned int level)
 #if defined(CONFIG_ARM_UNWIND)
 #warning "TODO: return_address should use unwind tables"
 #endif
-
+/*
 void *return_address(unsigned int level)
 {
 	return NULL;
 }
-
+*/
 #endif /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) / else */
 
 EXPORT_SYMBOL_GPL(return_address);
